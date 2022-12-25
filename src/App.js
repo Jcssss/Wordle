6e6159f2a85f5c62211a50085a69892f7aa47690
Justin Siu <justinsiu@uvic.ca>
2022-12-24 20:31:48 -0800

    changed the code a little

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Keyboard from './components/Keyboard';
 import Menu from './components/Menu';
 import WordGrid from './components/WordGrid';
 import raw from './dict.txt';
 
 const App = () => {
-    
-    const compliments = ["Yipee", "Congrats", "Well Done" , "You're the best", "Way to go", "Winner", "You're a genius", "Damn", "Keep up the good work!", "You're a star"];
     const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     const [message, setMessage] = useState("");
     const [gameState, setGameState] = useState(1);
@@ -42,28 +40,45 @@ const App = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [column, gameNum]);
 
+    /* initDict
+     * initializes the game's dictionary
+     */
+    const initDict = useCallback(() => {
+        fetch(raw)
+            .then(raw => raw.text())
+            .then(text => text.toUpperCase())
+            .then(text => text.replace(/\r/gm, ''))
+            .then(text => setDict(text.split("\n")));
+    }, []);
+
     // Initializes dictionary
     useEffect(() => {
         initDict();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [initDict]);
+
+    /* chooseWord
+     * chooses a new word for players to guess
+     */
+    const chooseWord = useCallback(() => {
+        var index = Math.floor(Math.random() * dict.length);
+        setChosenWord(dict[index]);
+    }, [dict]);
 
     // Chooses a word for the first game on page load
     useEffect(() => {
         chooseWord();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [dict]);
+    }, [dict, chooseWord]);
 
     // Checks if the player has lost whenever the row number updates
     useEffect(() => {
         if (gameState !== 2 && row === 6) {
             setGameState(0);
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [row]);
+    }, [row, gameState]);
 
     // Updates the message whenever the gameState is updated
     useEffect(() => {
+        const compliments = ["Yipee", "Congrats", "Well Done" , "You're the best", "Way to go", "Winner", "You're a genius", "Damn", "Keep up the good work!", "You're a star"];
         if (gameState === 0) {
             setMessage("Game Over! The word was '" + chosenWord + "'.");
         } else if (gameState === 2) {
@@ -72,19 +87,7 @@ const App = () => {
         } else {
             setMessage("");
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [gameState]);
-
-    /* initDict
-     * initializes the game's dictionary
-     */
-    const initDict = () => {
-        fetch(raw)
-            .then(raw => raw.text())
-            .then(text => text.toUpperCase())
-            .then(text => text.replace(/\r/gm, ''))
-            .then(text => setDict(text.split("\n")));
-    };
+    }, [gameState, chosenWord]);
 
     /* detectKey
      * handles key input
@@ -99,14 +102,6 @@ const App = () => {
         } else if (value === "Enter") {
             checkWord();
         }
-    };
-
-    /* chooseWord
-     * chooses a new word for players to guess
-     */
-    const chooseWord = () => {
-        var index = Math.floor(Math.random() * dict.length);
-        setChosenWord(dict[index]);
     };
 
     /* resetGrid
@@ -189,76 +184,82 @@ const App = () => {
     const checkWord = () => {
 
         // Ensures that a 5 letter word has been given
-        if (gameState === 1 && column === 5) {
+        if (!(gameState === 1 && column === 5)) {
+            return;
+        }
 
-            var inputWord = words[row];
-            var guessWord = chosenWord.split('');
-            var letterValsCopy = letterVals.slice();
-            var inputWordValues = [0,0,0,0,0];
+        var inputWord = words[row];
+        var guessWord = chosenWord.split('');
+        var letterValsCopy = letterVals.slice();
+        var inputWordValues = [0,0,0,0,0];
 
-            var valuesCopy = values.map((val) => {
-                return val.slice();
-            });
+        var valuesCopy = values.map((val) => {
+            return val.slice();
+        });
 
-            // Tracks the number of each letter in the two words
-            var inputLetterCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-            var guessLetterCounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        // Tracks the number of each letter in the two words
+        var inputLetterCounts = [];
+        inputLetterCounts.length = 26;
+        inputLetterCounts.fill(0);
 
-            for (var j = 0; j < guessWord.length; j++) {
-                inputLetterCounts[letters.indexOf(inputWord[j])]++;
-                guessLetterCounts[letters.indexOf(guessWord[j])]++;
+        var guessLetterCounts = [];
+        guessLetterCounts.length = 26;
+        guessLetterCounts.fill(0);
+
+        for (var j = 0; j < guessWord.length; j++) {
+            inputLetterCounts[letters.indexOf(inputWord[j])]++;
+            guessLetterCounts[letters.indexOf(guessWord[j])]++;
+        }
+
+        // Checks that the inputted word is valid
+        if (dict.includes(inputWord.join(""))) {
+
+            // Checks each letter in the inputted word
+            for (var i = 0; i < inputWord.length; i++) {
+
+                // If the letter is in the correct spot
+                if (inputWord[i] === guessWord[i]) {
+
+                    // updates it's values
+                    inputWordValues[i] = 3;
+                    letterValsCopy[letters.indexOf(inputWord[i])] = Math.max(letterValsCopy[letters.indexOf(inputWord[i])], 3);
+
+                // If the letter is in the wrong spot, and it's not possible to have extra letters
+                } else if (guessWord.includes(inputWord[i]) && inputLetterCounts[letters.indexOf(inputWord[i])] <= guessLetterCounts[letters.indexOf(inputWord[i])]) {
+                    
+                    // updates it's values
+                    inputWordValues[i] = 2;
+                    letterValsCopy[letters.indexOf(inputWord[i])] = Math.max(letterValsCopy[letters.indexOf(inputWord[i])], 2);
+                
+                // If the letter is in the wrong spot, and it's possible to have extra letters
+                } else if (guessWord.includes(inputWord[i])) {
+                    
+                    // decrease that letters count
+                    inputLetterCounts[letters.indexOf(inputWord[i])]--;
+                    
+                    // update it's values
+                    inputWordValues[i] = 1;
+                    letterValsCopy[letters.indexOf(inputWord[i])] = Math.max(letterValsCopy[letters.indexOf(inputWord[i])], 0);
+
+                // If the letter is not in the final word
+                } else {
+
+                    // update it's values
+                    inputWordValues[i] = 1;
+                    letterValsCopy[letters.indexOf(inputWord[i])] = Math.max(letterValsCopy[letters.indexOf(inputWord[i])], 1);
+                }
             }
 
-            // Checks that the inputted word is valid
-            if (dict.includes(inputWord.join(""))) {
+            // update state variables
+            valuesCopy[row] = inputWordValues;
+            setValues(valuesCopy);
+            setLetterVals(letterValsCopy);
 
-                // Checks each letter in the inputted word
-                for (var i = 0; i < inputWord.length; i++) {
+            setRow(row => row + 1);
+            setColumn(0);
 
-                    // If the letter is in the correct spot
-                    if (inputWord[i] === guessWord[i]) {
-
-                        // updates it's values
-                        inputWordValues[i] = 3;
-                        letterValsCopy[letters.indexOf(inputWord[i])] = Math.max(letterValsCopy[letters.indexOf(inputWord[i])], 3);
-
-                    // If the letter is in the wrong spot, and it's not possible to have extra letters
-                    } else if (guessWord.includes(inputWord[i]) && inputLetterCounts[letters.indexOf(inputWord[i])] <= guessLetterCounts[letters.indexOf(inputWord[i])]) {
-                        
-                        // updates it's values
-                        inputWordValues[i] = 2;
-                        letterValsCopy[letters.indexOf(inputWord[i])] = Math.max(letterValsCopy[letters.indexOf(inputWord[i])], 2);
-                    
-                    // If the letter is in the wrong spot, and it's possible to have extra letters
-                    } else if (guessWord.includes(inputWord[i])) {
-                        
-                        // decrease that letters count
-                        inputLetterCounts[letters.indexOf(inputWord[i])]--;
-                        
-                        // update it's values
-                        inputWordValues[i] = 1;
-                        letterValsCopy[letters.indexOf(inputWord[i])] = Math.max(letterValsCopy[letters.indexOf(inputWord[i])], 0);
-
-                    // If the letter is not in the final word
-                    } else {
-
-                        // update it's values
-                        inputWordValues[i] = 1;
-                        letterValsCopy[letters.indexOf(inputWord[i])] = Math.max(letterValsCopy[letters.indexOf(inputWord[i])], 1);
-                    }
-                }
-
-                // update state variables
-                valuesCopy[row] = inputWordValues;
-                setValues(valuesCopy);
-                setLetterVals(letterValsCopy);
-
-                setRow(row => row + 1);
-                setColumn(0);
-
-                if (inputWord.join("") === chosenWord) {
-                    setGameState(2);
-                }
+            if (inputWord.join("") === chosenWord) {
+                setGameState(2);
             }
         }
     }
